d0c8b987d92e6e2479372c4d2d23c49e90cc1101
ZumDeWald <zumdewald@gmail.com>
2019-05-28 22:30:01 -0500

    Add react-testing-lib and setup to cleanup forEach

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
     //Allows load and test of CSS files in Node with Jest
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  //after Jest is loaded
+  setupFilesAfterEnv: [require.resolve('./test/setup-tests.js')],
 };
